46f08be058876b7b1c1c9c1442bcf601458239ca
Danny Garcia Cortes <dannygarciadev@gmail.com>
2023-10-26 14:21:07 -0400

    adding files

--- js/profile.js
+++
@@ -1,6 +0,0 @@
-const hamburgerButton = document.querySelector(".hamburger-button");
-const menu = document.querySelector(".menu");
-
-hamburgerButton.addEventListener("click", () => {
-    menu.classList.toggle("menu-active");
-});
